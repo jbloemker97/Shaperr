e03c3704807578afae31acf04b3b4b57a5cea169
Jacob <jbloemker97@gmail.com>
2017-11-09 08:34:31 -0500

    Finished

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Responsive Nav
 function myFunction() {
     var x = document.getElementById("myTopNav");
     if (x.className === "topnav") {
@@ -6,4 +7,70 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+// Pop ups
+
+
+//Grab Buttons
+let aboutUs = document.getElementById("about");
+let aboutUsNav = document.getElementById("about-nav");
+let displayAboutUs = document.querySelector(".about-us-content");
+
+let insights = document.getElementById("insights");
+let insightsNav = document.getElementById("insights-nav");
+let displayInsights = document.querySelector(".insights-content");
+
+let people = document.getElementById("people");
+let peopleNav = document.getElementById("people-nav");
+let displayPeople = document.querySelector(".people-content");
+
+
+
+
+let closeButton = document.getElementById("close-about");
+let closeInsights = document.getElementById("close-insights");
+let closePeople = document.getElementById("close-people");
+
+
+// Event Listeners 
+//About Us
+aboutUs.addEventListener("click", function(){
+    displayAboutUs.style.display = "flex";
+});
+
+aboutUsNav.addEventListener("click", function(){
+    displayAboutUs.style.display = "flex";
+});
+
+
+
+closeButton.addEventListener("click", function(){
+    displayAboutUs.style.display = "none";
+});
+
+// Insights 
+insights.addEventListener("click", function(){
+    displayInsights.style.display = "flex";
+});
+
+insightsNav.addEventListener("click", function(){
+    displayInsights.style.display = "flex";
+});
+
+closeInsights.addEventListener("click", function(){
+    displayInsights.style.display = "none";
+});
+
+//People
+people.addEventListener("click", function(){
+    displayPeople.style.display = "flex";
+});
+
+peopleNav.addEventListener("click", function(){
+    displayPeople.style.display = "flex";
+});
+
+closePeople.addEventListener("click", function(){
+    displayPeople.style.display = "none";
+})
 
